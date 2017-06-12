39178e56f9c101ef221b5963c92a9569bd860066
huangyichun <576305972@qq.com>
2017-06-12 22:18:56 +0800

    秒杀实体类

---
+++ src/main/java/com/seckill/mode/Seckill.java
@@ -0,0 +1,81 @@
+package com.seckill.mode;
+
+import java.util.Date;
+
+/**
+ * 秒杀实体
+ */
+public class Seckill {
+
+    private long seckillId;
+
+    private String name;
+
+    private int number;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    private Date creatTime;
+
+    public long getSeckillId() {
+        return seckillId;
+    }
+
+    public void setSeckillId(long seckillId) {
+        this.seckillId = seckillId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Seckill{" +
+                "seckillId=" + seckillId +
+                ", name='" + name + '\'' +
+                ", number=" + number +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", creatTime=" + creatTime +
+                '}';
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Date getCreatTime() {
+        return creatTime;
+    }
+
+    public void setCreatTime(Date creatTime) {
+        this.creatTime = creatTime;
+    }
+}
