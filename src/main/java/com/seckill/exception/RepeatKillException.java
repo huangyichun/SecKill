20f3f25eae2d490b7d8991fde4b964b7485e0a43
huangyichun <576305972@qq.com>
2017-06-14 21:10:27 +0800

    重复秒杀异常

---
+++ src/main/java/com/seckill/exception/RepeatKillException.java
@@ -0,0 +1,17 @@
+package com.seckill.exception;
+
+import com.seckill.dto.SeckillExecution;
+
+/**
+ * 重复秒杀异常(运行期异常)
+ */
+public class RepeatKillException extends SeckillExecution{
+
+    public RepeatKillException(String message) {
+        super(message);
+    }
+
+    public RepeatKillException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
