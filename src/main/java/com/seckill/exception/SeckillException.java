3793f429fa6e6dd5bea01fa3f283ff5c1c668515
huangyichun <576305972@qq.com>
2017-06-14 21:21:21 +0800

    修改错误

--- src/main/java/com/seckill/exception/SeckillException.java
+++ src/main/java/com/seckill/exception/SeckillException.java
@@ -6,7 +6,7 @@ import com.seckill.dto.SeckillExecution;
  * 秒杀相关业务异常
  * Created by huangyichun on 2017/6/14.
  */
-public class SeckillException extends SeckillExecution{
+public class SeckillException extends RuntimeException{
 
     public SeckillException(String message) {
         super(message);
