4924963dbf43793ad48945227d7f2e04c7c35037
huangyichun <576305972@qq.com>
2017-06-14 21:10:13 +0800

    所有秒杀异常父类

---
+++ src/main/java/com/seckill/exception/SeckillException.java
@@ -0,0 +1,18 @@
+package com.seckill.exception;
+
+import com.seckill.dto.SeckillExecution;
+
+/**
+ * 秒杀相关业务异常
+ * Created by huangyichun on 2017/6/14.
+ */
+public class SeckillException extends SeckillExecution{
+
+    public SeckillException(String message) {
+        super(message);
+    }
+
+    public SeckillException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
