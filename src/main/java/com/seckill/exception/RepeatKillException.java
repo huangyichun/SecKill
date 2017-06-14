2ac2bf76c2c3ed54e0a26ffb88140fca5a67506a
huangyichun <576305972@qq.com>
2017-06-14 21:18:05 +0800

    修改了继承错误

--- src/main/java/com/seckill/exception/RepeatKillException.java
+++ src/main/java/com/seckill/exception/RepeatKillException.java
@@ -5,7 +5,7 @@ import com.seckill.dto.SeckillExecution;
 /**
  * 重复秒杀异常(运行期异常)
  */
-public class RepeatKillException extends SeckillExecution{
+public class RepeatKillException extends SeckillException{
 
     public RepeatKillException(String message) {
         super(message);
